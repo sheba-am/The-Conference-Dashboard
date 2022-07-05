9c4994f7ceead6b7e52fa9b040676637ecd3bf8e
sheba <68592737+sheba-am@users.noreply.github.com>
2022-07-05 20:33:03 +0430

    second

---
+++ conference-dashboard-react/src/components/AdminDashboard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function AdminDashboard(){
+    return(
+        <div>
+            <p>admin Dashboard?j!</p>
+            <button type="button" class="btn btn-primary">Primary</button>
+            
+        </div>
+    )
+}
