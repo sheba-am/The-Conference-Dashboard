f1e612954adc8273c6574e016490102ba29c77d7
sheba <68592737+sheba-am@users.noreply.github.com>
2022-07-05 21:03:41 +0430

    third

--- conference-dashboard-react/src/App.js
+++ conference-dashboard-react/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import AdminDashboard from './components/AdminDashboard';
+import Sidebar from './components/Sidebar';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="container">
+        <div class="row">
+          <div class="col-9"><AdminDashboard /></div>
+          <div class="col-2" className="sidebar"><Sidebar /></div>
+        </div>
+      </div>
+        
+        
     </div>
   );
 }
--- conference-dashboard-react/src/components/Sidebar.js
+++ conference-dashboard-react/src/components/Sidebar.js
@@ -3,7 +3,8 @@ import React from "react";
 export default function Sidebar(){
     return(
         <div>
-            <p>sidebar </p>
+            <p>sidebar! </p>
+            <button type="button" class="btn btn-primary">side</button>
         </div>
     )
 }
